dc495b18598aae28caa9b39e86bb1f6a8d8f4657
IRSINGH27 <irsingh@ncbs.res.in>
2023-03-20 17:31:22 +0530

    started

--- __module__/__test__.py
+++ __module__/__test__.py
@@ -23,12 +23,16 @@ gtcaacaaaggcacctcggcgctgaagcgcctgcccaagaaaggcaagtaa'''.replace('\n','')
 seq_0=Seq(seq_id,)
 seq_0.translate()
 p=0.2
-p_dict={'a':{'a':1-3*p,'t':p,'g':p,'c':p},
-        't':{'a':p,'t':1-3*p,'g':p,'c':p},
-        'g':{'a':p,'t':p,'g':1-3*p,'c':p},
-        'c':{'a':p,'t':p,'g':p,'c':1-3*p}}
+q=0.3
+subsitute_dict={'a':{'a':1-2*p-q,'t':p,'g':q,'c':p},
+        't':{'a':p,'t':1-2*p-q,'g':p,'c':q},
+        'g':{'a':q,'t':p,'g':1-2*p-q,'c':p},
+        'c':{'a':p,'t':q,'g':p,'c':1-2*p-q}}
+
+indel={'ins':{'a':0.25,'t':0.25,'g':0.25,'c':0.25},'del':{'a':0.25,'t':0.25,'g':0.25,'c':0.25}}
+
 
 _temp_seq=seq_0[3:-3]
 pos=round(np.random.uniform(low=0,high=len(_temp_seq)))
 _nt=_temp_seq[pos]
-change=np.random.choice(p_dict[_nt.lower()],p)
+change=np.random.choice(subsitute_dict[_nt.lower()],p)
