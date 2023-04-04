93e38aa5c863b312689399846b77663fd805f6d5
IRSINGH27 <inderrajasr@gmail.com>
2023-04-04 14:58:17 +0530

    rep increase

--- __module__/multi_class.py
+++ __module__/multi_class.py
@@ -2,7 +2,7 @@ from __test__2 import main
 def multi_function():
     from multiprocessing import Process
     _p=[]
-    for p in range(4):
+    for p in range(10):
       _p.append(Process(target=main,args=(0.01,0.02,p)))
     for i in _p:
         i.start()
